7e9d07b1009719e10ea1d93881eb2ebbc1147c4e
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-04-18 17:16:42 +0200

    Update creationCommande

--- src/main/java/tp/javaee/servlets/CreationCommande.java
+++ src/main/java/tp/javaee/servlets/CreationCommande.java
@@ -1,13 +1,17 @@
 package tp.javaee.servlets;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import tp.javaee.beans.Client;
 import tp.javaee.beans.Commande;
 import tp.javaee.forms.CreationCommandeForm;
 
@@ -21,6 +25,8 @@ public class CreationCommande extends HttpServlet {
 	/* Constantes */
 	public static final String ATT_COMMANDE = "commande";
     public static final String ATT_FORM     = "form";
+    public static final String SESSION_CLIENTS   = "clients";
+    public static final String SESSION_COMMANDES = "commandes";
 
     public static final String VUE_SUCCES   = "/WEB-INF/afficherCommande.jsp";
     public static final String VUE_FORM     = "/WEB-INF/creerCommande.jsp";
@@ -56,6 +62,28 @@ public class CreationCommande extends HttpServlet {
         request.setAttribute( ATT_FORM, form );
 
         if ( form.getErreurs().isEmpty() ) {
+        	/* Alors récupération de la map des clients dans la session */
+            HttpSession session = request.getSession();
+            Map<String, Client> clients = (HashMap<String, Client>) session.getAttribute( SESSION_CLIENTS );
+            /* Si aucune map n'existe, alors initialisation d'une nouvelle map */
+            if ( clients == null ) {
+                clients = new HashMap<String, Client>();
+            }
+            /* Puis ajout du client de la commande courante dans la map */
+            clients.put( commande.getClient().getNom(), commande.getClient() );
+            /* Et enfin (ré)enregistrement de la map en session */
+            session.setAttribute( SESSION_CLIENTS, clients );
+ 
+            /* Ensuite récupération de la map des commandes dans la session */
+            Map<String, Commande> commandes = (HashMap<String, Commande>) session.getAttribute( SESSION_COMMANDES );
+            /* Si aucune map n'existe, alors initialisation d'une nouvelle map */
+            if ( commandes == null ) {
+                commandes = new HashMap<String, Commande>();
+            }
+            /* Puis ajout de la commande courante dans la map */
+            commandes.put( commande.getDate(), commande );
+            /* Et enfin (ré)enregistrement de la map en session */
+            session.setAttribute( SESSION_COMMANDES, commandes );
             /* Si aucune erreur, alors affichage de la fiche récapitulative */
             this.getServletContext().getRequestDispatcher( VUE_SUCCES ).forward( request, response );
         } else {
