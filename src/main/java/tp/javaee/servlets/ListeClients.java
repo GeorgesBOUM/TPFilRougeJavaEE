3d29b29c7d736a18c0ef90cdc431cf667329f352
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-04-18 17:17:32 +0200

    Create ListeClients

---
+++ src/main/java/tp/javaee/servlets/ListeClients.java
@@ -0,0 +1,44 @@
+package tp.javaee.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListeClient
+ */
+@WebServlet(name = "listeClients", urlPatterns = { "/listeClients" })
+public class ListeClients extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public static final String ATT_CLIENT = "client";
+    public static final String ATT_FORM   = "form";
+
+    public static final String VUE        = "/WEB-INF/listerClients.jsp";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListeClients() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher(VUE).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
