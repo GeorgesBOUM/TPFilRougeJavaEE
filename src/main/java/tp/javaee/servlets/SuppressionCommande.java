20b569044aa910769dcade6c9a106cfbc7ad4f06
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-05-03 19:24:14 +0200

    update SuppressionCommande

--- src/main/java/tp/javaee/servlets/SuppressionCommande.java
+++ src/main/java/tp/javaee/servlets/SuppressionCommande.java
@@ -12,49 +12,54 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import tp.javaee.beans.Commande;
+import tp.javaee.dao.CommandeDao;
+import tp.javaee.dao.DAOException;
+import tp.javaee.dao.DAOFactory;
 
 /**
  * Servlet implementation class SuppressionCommande
  */
 @WebServlet(name = "suppressionCommande", urlPatterns = { "/suppressionCommande" })
 public class SuppressionCommande extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	public static final String PARAM_DATE_COMMANDE = "dateCommande";
-    public static final String SESSION_COMMANDES   = "commandes";
+	public static final String CONF_DAO_FACTORY  = "daofactory";
+    public static final String PARAM_ID_COMMANDE = "idCommande";
+    public static final String SESSION_COMMANDES = "commandes";
 
-    public static final String VUE                 = "/listeCommandes";
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SuppressionCommande() {
-        super();
-        // TODO Auto-generated constructor stub
+    public static final String VUE               = "/listeCommandes";
+
+    private CommandeDao        commandeDao;
+
+    public void init() throws ServletException {
+        /* Récupération d'une instance de notre DAO Utilisateur */
+        this.commandeDao = ( (DAOFactory) getServletContext().getAttribute( CONF_DAO_FACTORY ) ).getCommandeDao();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/* Récupération du paramètre */
-        String dateCommande = getValeurParametre( request, PARAM_DATE_COMMANDE );
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* Récupération du paramètre */
+        String idCommande = getValeurParametre( request, PARAM_ID_COMMANDE );
+        Long id = Long.parseLong( idCommande );
 
         /* Récupération de la Map des commandes enregistrées en session */
         HttpSession session = request.getSession();
-        Map<String, Commande> commandes = (HashMap<String, Commande>) session.getAttribute( SESSION_COMMANDES );
+        Map<Long, Commande> commandes = (HashMap<Long, Commande>) session.getAttribute( SESSION_COMMANDES );
 
-        /* Si la date de la commande et la Map des commandes ne sont pas vides */
-        if ( dateCommande != null && commandes != null ) {
-            /* Alors suppression de la commande de la Map */
-            commandes.remove( dateCommande );
+        /* Si l'id de la commande et la Map des commandes ne sont pas vides */
+        if ( id != null && commandes != null ) {
+            try {
+                /* Alors suppression de la commande de la BDD */
+                commandeDao.supprimer( commandes.get( id ) );
+                /* Puis suppression de la commande de la Map */
+                commandes.remove( id );
+            } catch ( DAOException e ) {
+                e.printStackTrace();
+            }
             /* Et remplacement de l'ancienne Map en session par la nouvelle */
             session.setAttribute( SESSION_COMMANDES, commandes );
         }
 
         /* Redirection vers la fiche récapitulative */
         response.sendRedirect( request.getContextPath() + VUE );
-	}
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
